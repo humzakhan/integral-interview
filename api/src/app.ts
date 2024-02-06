9262931aa63d1a84a50c62d401b0930bc479efac
Humza Khan <humzakhan@outlook.com>
2024-02-06 18:42:45 -0500

    feat: add rate limiter

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,8 +5,16 @@ import cors from 'cors';
 import { config } from './config';
 import { routes } from './routes';
 import { errorHandler } from './middleware';
+import { rateLimit } from 'express-rate-limit'
 
 const app: Express = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per window
+});
+
+app.use(limiter);
 
 app.use(helmet());
 
